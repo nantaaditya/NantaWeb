69e11dc18cd85a3dcc3ff5f1c4ecce2a64cbbf61
Pramuditya Anantanur <pramuditza@gmail.com>
2018-02-26 13:01:17 +0700

    Main App

---
+++ src/main/java/com/nanta/NantaApplication.java
@@ -0,0 +1,36 @@
+package com.nanta;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class NantaApplication extends SpringBootServletInitializer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NantaApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(NantaApplication.class);
+  }
+
+  @Bean
+  public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+    tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+      if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+        ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+      }
+    });
+    return tomcat;
+  }
+}
