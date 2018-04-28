a392779521e7ec76fbf10f49d9c57f0ddad8947a
Pramuditya Anantanur <pramuditza@gmail.com>
2018-04-28 18:52:01 +0700

    add CacheTopics

---
+++ src/main/java/com/nanta/base/CacheTopics.java
@@ -0,0 +1,11 @@
+package com.nanta.base;
+
+public interface CacheTopics {
+  String ABOUT = "personalInformation";
+  String BLOG = "blog";
+  String EMAIL = "email";
+  String EDUCATION = "education";
+  String IMAGE = "image";
+  String PROJECT = "project";
+  String SKILL = "skill";
+}
