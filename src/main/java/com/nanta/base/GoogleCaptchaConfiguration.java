bbcb2762d58d1808f2bf61d580e1be90d8ea4da8
Pramuditya Anantanur <pramuditza@gmail.com>
2018-04-28 18:51:16 +0700

    rename BaseConfiguration to GoogleCaptchaConfiguration

--- src/main/java/com/nanta/base/GoogleCaptchaConfiguration.java
+++ src/main/java/com/nanta/base/GoogleCaptchaConfiguration.java
@@ -4,17 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BaseConfiguration {
+public class GoogleCaptchaConfiguration {
   @Value("${google.captcha.private.key}")
   private String CAPTCHA_PRIVATE_KEY;
-  @Value("${application.base.url}")
-  private String BASE_URL;
 
   public String getCaptchaPrivateKey() {
     return this.CAPTCHA_PRIVATE_KEY;
-  }
-
-  public String getBaseUrl() {
-    return this.BASE_URL;
   }
 }
