26f260344fd314f7b0ca0cd9c9f8d00099578951
Pramuditya Anantanur <pramuditza@gmail.com>
2018-05-13 00:22:40 +0700

    add RegexMatchers

---
+++ src/main/java/com/nanta/util/RegexMatchers.java
@@ -0,0 +1,30 @@
+package com.nanta.util;
+
+import java.util.regex.Pattern;
+
+import com.nanta.base.RegexType;
+
+public class RegexMatchers {
+
+  private static final Pattern URL = Pattern.compile(
+      "(?i)^(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))\\.?)(?::\\d{2,5})?(?:[/?#]\\S*)?$");
+  private static final Pattern EMAIL =
+      Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+  public static boolean isValid(RegexType type, String text) {
+    boolean isValid = false;
+    switch (type) {
+      case URL:
+        isValid = URL.matcher(text).matches();
+        break;
+      case EMAIL:
+        isValid = EMAIL.matcher(text).matches();
+        break;
+      default:
+        isValid = false;
+        break;
+    }
+    return isValid;
+  }
+}
+
